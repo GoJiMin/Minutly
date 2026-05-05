140fa723e16e255993f60166fc3a981a68f4eccf
Prolip <gojimin3095@gmail.com>
2026-05-05 23:49:40 +0900

    [refactor] 요청 함수들을 명시적으로 비동기 함수로 보이게 수정

--- src/entities/auth/api/authApi.ts
+++ src/entities/auth/api/authApi.ts
@@ -2,27 +2,27 @@ import {LoginRequest} from '../model/schema';
 import {AuthMeResponse} from '../model/types';
 import {fetchGet, fetchPost} from '@/shared/api';
 
-export function fetchLogin(input: LoginRequest) {
-  fetchPost({
+export async function fetchLogin(input: LoginRequest) {
+  await fetchPost({
     endpoint: '/api/auth/login',
     body: input,
   });
 }
 
-export function fetchLogout() {
-  fetchPost({
+export async function fetchLogout() {
+  await fetchPost({
     endpoint: '/api/auth/logout',
   });
 }
 
-export function fetchTokenRefresh() {
-  fetchPost({
+export async function fetchTokenRefresh() {
+  await fetchPost({
     endpoint: '/api/auth/refresh',
   });
 }
 
-export function fetchAuthMe() {
-  return fetchGet<AuthMeResponse>({
+export async function fetchAuthMe() {
+  return await fetchGet<AuthMeResponse>({
     endpoint: '/api/auth/me',
     errorHandlingType: 'silent',
   });
