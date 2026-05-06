91ea4812dcd5266828aef4d7b8b0f7d70bb387a1
Prolip <gojimin3095@gmail.com>
2026-05-06 23:19:11 +0900

    [test] 로그인 폼 테스트 작성

---
+++ src/shared/utils/withAllContext.tsx
@@ -0,0 +1,17 @@
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+export function withAllContext(children: React.ReactNode) {
+  const testQueryClient = createTestQueryClient();
+
+  return <QueryClientProvider client={testQueryClient}>{children}</QueryClientProvider>;
+}
+
+function createTestQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: 0,
+      },
+    },
+  });
+}
