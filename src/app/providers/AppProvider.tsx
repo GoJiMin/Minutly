8812e5eafc242a4d3b1c61e39d1a3cd94005a523
Prolip <gojimin3095@gmail.com>
2026-05-06 21:19:01 +0900

    [feat] 전역 에러를 구독하고, 예측 가능한 서버 에러일 경우 토스트 표시, 예측하지 못한 에러는 전역 에러 바운더리로 넘기도록 구현

--- src/app/providers/AppProvider.tsx
+++ src/app/providers/AppProvider.tsx
@@ -1,12 +1,30 @@
 import {PropsWithChildren} from 'react';
+import {Toaster as SonnerToaster} from 'sonner';
 import {ReactQueryProvider} from './ReactQueryProvider';
+import {GlobalErrorDetector} from './GlobalErrorDetector';
+import {UnexpectedErrorBoundary} from './UnexpectedErrorBoundary';
 import {AuthBootstrapper} from '@/features/auth';
 
 export function AppProvider({children}: PropsWithChildren) {
   return (
-    <ReactQueryProvider>
-      <AuthBootstrapper />
-      {children}
-    </ReactQueryProvider>
+    <UnexpectedErrorBoundary>
+      <ReactQueryProvider>
+        <AuthBootstrapper />
+        <SonnerToaster
+          position="top-center"
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+          toastOptions={{
+            style: {width: 'fit-content'},
+          }}
+          duration={4000}
+          visibleToasts={4}
+        />
+        <GlobalErrorDetector />
+        {children}
+      </ReactQueryProvider>
+    </UnexpectedErrorBoundary>
   );
 }
