fa0b74105ee1f3c4b90d729dbba3d08acd2d4aca
Prolip <gojimin3095@gmail.com>
2026-05-06 21:17:49 +0900

    [feat] 예측 가능한 서버 에러 판단 유틸 및 합의한 서버 에러 코드를 매핑한 메세지 객체 추가

---
+++ src/shared/config/errorMessage.ts
@@ -0,0 +1,23 @@
+type ErrorMessage = Record<string, string>;
+
+export const SERVER_ERROR_MESSAGE: ErrorMessage = {
+  /**
+   * 공통
+   */
+  UNAUTHORIZED: '인증이 필요한 기능이에요.',
+  TOKEN_EXPIRED: '인증 정보가 만료됐어요. 다시 로그인해주세요.',
+  INVALID_REQUEST: '잘못된 요청이에요. 다시 시도해주세요.',
+
+  /**
+   * 인증 관련
+   */
+  // POST /api/auth/login
+  INVALID_CREDENTIALS: '로그인 정보가 잘못됐어요. 다시 시도해주세요.',
+  AUTH_LOGIN_FAILED: '로그인 처리 중 문제가 발생했어요. 다시 시도해주세요.',
+
+  // POST /api/auth/refresh
+  AUTH_REFRESH_FAILED: '인증 정보 갱신에 실패했어요. 다시 시도해주세요.',
+
+  // POST /api/auth/logout
+  AUTH_LOGOUT_FAILED: '로그아웃에 실패했어요. 다시 시도해주세요.',
+} as const;
