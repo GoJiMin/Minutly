c96c4c4f5146531f7a4074d9df24c25b0cdcedb2
Prolip <gojimin3095@gmail.com>
2026-05-19 20:34:39 +0900

    [feat] 선택된 날짜가 없을 경우 표시하는 안내 컴포넌트 구현

--- src/features/meeting-history/ui/side-bar/MeetingHistorySidebar.tsx
+++ src/features/meeting-history/ui/side-bar/MeetingHistorySidebar.tsx
@@ -1,15 +1,16 @@
 import {Suspense} from 'react';
 import {MeetingCalendar} from './MeetingCalendar';
 import {MeetingListBySelectedDate} from './MeetingListBySelectDate';
+import {Separator} from '@/shared/components';
 
 export function MeetingHistorySidebar() {
   return (
-    <aside className="w-96 flex flex-col items-center">
+    <aside className="w-96 flex flex-col items-center gap-3">
       {/* TODO: 로딩 UI 및 에러 바운더리 폴백 UI 구현 */}
       <Suspense fallback={<div>loading</div>}>
         <MeetingCalendar />
       </Suspense>
-
+      <Separator />
       {/* TODO: 로딩 UI 및 에러 바운더리 폴백 UI 구현 */}
       <Suspense fallback={<div>loading</div>}>
         <MeetingListBySelectedDate />
